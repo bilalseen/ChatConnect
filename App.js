eea1a7c2a915a971e151069414e5fd2327b58632
bilalseen <bilalsen2010@gmail.com>
2024-09-26 20:39:50 +0300

    flash message added

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
+import FlashMessage from "react-native-flash-message";
 import Router from "./src/navigation/Router";
 
 export default function App() {
-  return <Router />;
+  return (
+    <>
+      <Router />
+      <FlashMessage position="top" />
+    </>
+  );
 }
