7815a198790449fcbe7cbbabd8368e754d418802
bilalseen <bilalsen2010@gmail.com>
2024-09-26 17:08:26 +0300

    Refactor Button component to use theme-based styling

---
+++ src/components/Button.js
@@ -0,0 +1,50 @@
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React from "react";
+import colors from "../styles/colors";
+
+export default function Button({ text, onPress, theme = "primary" }) {
+  return (
+    <TouchableOpacity style={styles[theme].container} onPress={onPress}>
+      <Text style={styles[theme].buttonText}>{text}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const buttonBaseStyle = {
+  container: {
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    height: 50,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontWeight: "600",
+    fontSize: 18,
+  },
+};
+
+const styles = {
+  primary: StyleSheet.create({
+    ...buttonBaseStyle,
+    container: {
+      ...buttonBaseStyle.container,
+      backgroundColor: colors.secondary,
+    },
+    buttonText: {
+      ...buttonBaseStyle.buttonText,
+      color: colors.tertiary,
+    },
+  }),
+  secondary: StyleSheet.create({
+    ...buttonBaseStyle,
+    container: {
+      ...buttonBaseStyle.container,
+      backgroundColor: colors.tertiary,
+    },
+    buttonText: {
+      ...buttonBaseStyle.buttonText,
+      color: colors.secondary,
+    },
+  }),
+};
