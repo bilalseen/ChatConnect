9e9e35862758e7fc30d06cc4c874c441f7780a2c
bilalseen <bilalsen2010@gmail.com>
2024-09-26 17:09:53 +0300

    Refactor SignIn page to use theme-based styling and add input fields and buttons for user authentication

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,10 +1,32 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
+import colors from "../styles/colors";
+import Input from "../components/auth/Input";
+import Button from "../components/Button";
 
-export default function SignIn() {
+export default function SignIn({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>SignIn</Text>
+      <View style={styles.titleContainer}>
+        <Text style={styles.titleText}>chatConnect</Text>
+      </View>
+      <View style={styles.formContainer}>
+        <View style={styles.inputContainer}>
+          <Input
+            placeholder="e-postanızı giriniz.."
+            keyboardType="email-address"
+          />
+          <Input placeholder="şifrenizi giriniz.." secureTextEntry={true} />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button text={"Giriş Yap"} />
+          <Button
+            theme="secondary"
+            text={"Kayıt Ol"}
+            onPress={() => navigation.navigate("SignUpScreen")}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -12,8 +34,28 @@ export default function SignIn() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: colors.primary,
+    justifyContent: "center",
+    paddingHorizontal: 10,
+  },
+  titleContainer: {
+    flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  titleText: {
+    color: colors.tertiary,
+    fontSize: 26,
+    fontWeight: "600",
+  },
+  formContainer: {
+    flex: 1,
+    gap: 50,
+  },
+  inputContainer: {
+    gap: 20,
+  },
+  buttonContainer: {
+    gap: 20,
   },
 });
