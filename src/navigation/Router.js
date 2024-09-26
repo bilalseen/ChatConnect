cada46f1f4736923f0af7fe8aae7217c0ec9bdc0
bilalseen <bilalsen2010@gmail.com>
2024-09-26 17:10:16 +0300

    Refactor Router component to conditionally render screens based on user authentication status

--- src/navigation/Router.js
+++ src/navigation/Router.js
@@ -3,26 +3,48 @@ import Home from "../pages/Home";
 import SignUp from "../pages/SignUp";
 import SignIn from "../pages/SignIn";
 import { NavigationContainer } from "@react-navigation/native";
+import colors from "../styles/colors";
+import { useState } from "react";
 
 const Stack = createNativeStackNavigator();
 
 export default function Router() {
+  const [user, setUser] = useState(null);
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="HomeScreen"
-          component={Home}
-          options={{
-            headerTitle: "Odalar",
-            headerStyle: { backgroundColor: "#f4511e" },
-            headerTintColor: "#fff",
-            headerTitleAlign: "center",
-          }}
-        />
-        <Stack.Screen name="SignUpScreen" component={SignUp} />
-        <Stack.Screen name="SignInScreen" component={SignIn} />
-      </Stack.Navigator>
+      {user ? (
+        <Stack.Navigator>
+          <Stack.Screen
+            name="HomeScreen"
+            component={Home}
+            options={{
+              headerTitle: "Odalar",
+              headerStyle: {
+                backgroundColor: colors.primary,
+              },
+              headerTintColor: colors.background,
+              headerTitleAlign: "center",
+            }}
+          />
+        </Stack.Navigator>
+      ) : (
+        <Stack.Navigator>
+          <Stack.Screen
+            name="SignInScreen"
+            component={SignIn}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="SignUpScreen"
+            component={SignUp}
+            options={{
+              headerShown: false,
+            }}
+          />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 }
