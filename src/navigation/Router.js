d862defc5baf752d3905f6c50a4ab4df28305af9
bilalseen <bilalsen2010@gmail.com>
2024-09-26 20:41:58 +0300

    Refactor Router component to conditionally render screens based on user authentication status and add sign out functionality

--- src/navigation/Router.js
+++ src/navigation/Router.js
@@ -1,15 +1,47 @@
+import { useState, useEffect } from "react";
+import { Text } from "react-native";
+
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+
 import Home from "../pages/Home";
 import SignUp from "../pages/SignUp";
 import SignIn from "../pages/SignIn";
-import { NavigationContainer } from "@react-navigation/native";
+
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../services/firebaseConfig";
+
 import colors from "../styles/colors";
-import { useState } from "react";
+import Entypo from "@expo/vector-icons/Entypo";
 
 const Stack = createNativeStackNavigator();
 
 export default function Router() {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const handleSignOut = () => {
+    showMessage({
+      message: "Başarılı",
+      description: "Çıkış Yapıldı",
+      type: "success",
+    });
+    auth.signOut();
+  };
+
+  if (loading) {
+    return <Text>Loading...</Text>;
+  }
+
   return (
     <NavigationContainer>
       {user ? (
@@ -24,6 +56,14 @@ export default function Router() {
               },
               headerTintColor: colors.background,
               headerTitleAlign: "center",
+              headerRight: () => (
+                <Entypo
+                  name="log-out"
+                  size={24}
+                  color="black"
+                  onPress={handleSignOut}
+                />
+              ),
             }}
           />
         </Stack.Navigator>
