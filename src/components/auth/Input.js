82f0b9f874ce83e2186887fb5007a429398ae694
bilalseen <bilalsen2010@gmail.com>
2024-09-26 17:09:38 +0300

    Refactor Input component to use theme-based styling

---
+++ src/components/auth/Input.js
@@ -0,0 +1,34 @@
+import { View, Text, TextInput, StyleSheet } from "react-native";
+import React from "react";
+import colors from "../../styles/colors";
+
+export default function Input({
+  placeholder,
+  secureTextEntry,
+  value,
+  onChangeText,
+  keyboardType,
+}) {
+  return (
+    <TextInput
+      onChangeText={onChangeText}
+      keyboardType={keyboardType}
+      placeholder={placeholder}
+      secureTextEntry={secureTextEntry}
+      value={value}
+      placeholderTextColor={colors.tertiary}
+      style={styles.container}
+      autoCapitalize="none"
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    borderColor: colors.tertiary,
+    borderBottomWidth: 1,
+    placeholderTextColor: colors.tertiary,
+    padding: 10,
+  },
+});
