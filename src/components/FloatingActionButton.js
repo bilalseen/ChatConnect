d3aeb1f30caf43cdc7c68a0b7292469d74eda689
bilalseen <bilalsen2010@gmail.com>
2024-09-27 17:12:30 +0300

    Add FloatingActionButton component for displaying a floating action button with a plus icon

---
+++ src/components/FloatingActionButton.js
@@ -0,0 +1,26 @@
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import React from "react";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import colors from "../styles/colors";
+
+export default function FloatingActionButton({ onPress }) {
+  return (
+    <TouchableOpacity style={styles.container} onPress={onPress}>
+      <AntDesign name="plus" size={24} color={colors.tertiary} />
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    position: "absolute",
+    bottom: 20,
+    right: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: colors.primary,
+    padding: 20,
+    borderRadius: 50,
+    zIndex: 1,
+  },
+});
