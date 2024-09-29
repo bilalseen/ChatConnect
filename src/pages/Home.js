c0ca6f56c11cd064522c2145fdeb3ffdacd33000
bilalseen <bilalsen2010@gmail.com>
2024-09-29 16:51:09 +0300

    Refactor Home.js to use @react-native-async-storage/async-storage package for user session management and add parseData utility function for parsing data into an array of objects

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,55 @@
-import { View, Text, StyleSheet, SafeAreaView } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet, SafeAreaView, FlatList } from "react-native";
+import React, { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
+import FloatingActionButton from "../components/FloatingActionButton";
+import RoomCard from "../components/RoomCard";
+import { getDatabase, onValue, ref } from "firebase/database";
+import parseData from "../utils/parseData";
 
 export default function Home() {
+  const [roomData, setRoomData] = useState([]);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const getRooms = () => {
+      const db = getDatabase();
+      const roomsRef = ref(db, "rooms");
+
+      const unsubscribe = onValue(
+        roomsRef,
+        (snapshot) => {
+          const data = snapshot.val();
+          if (data) {
+            const formattedData = parseData(data);
+            setRoomData(formattedData);
+          } else {
+            console.error("Veri bulunamadı");
+          }
+        },
+        (error) => {
+          console.error("Veri çekme hatası:", error);
+        }
+      );
+
+      return unsubscribe;
+    };
+
+    getRooms();
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar />
-      <Text>Home</Text>
+      <FloatingActionButton onPress={() => null} />
+      <FlatList
+        numColumns={2}
+        data={roomData}
+        renderItem={(item) => <RoomCard roomData={item.item} />}
+        keyExtractor={(item) => item.id}
+        showsVerticalScrollIndicator={false}
+        ListFooterComponent={<View style={{ height: 50 }} />}
+        ListHeaderComponent={<View style={{ height: 50 }} />}
+      />
     </SafeAreaView>
   );
 }
